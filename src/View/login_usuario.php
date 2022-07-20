aba21c7a223614925b063b5c501067f01f3a8255
Ygor <ygorsramos99@gmail.com>
2022-07-19 21:36:07 -0300

    TelasProjFinal

---
+++ src/View/login_usuario.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pr-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../css/main.css">
+        <title>Tela Login Usuário</title>
+
+    <body>
+        <header>
+            <nav>
+                <section id="logo"><img src="../img/logo_1.png" alt=""></section>
+                <article class="icones2">
+                    <img src="../img/pesquisar.png" alt="">
+                    <img src="../img/carrinho.png" alt="">
+                    <img src="../img/perfil.png" alt="">
+                </article>
+            </nav>
+        </header>
+        <section>
+            <h1 class="Acessar-conta">Acessar conta</h1>
+            <div class="login">
+                <h1 id="login">Login</h1>
+                <input id="email" type="text" placeholder="E-mail">
+                <br><br>
+                <input id="senha" type="password" placeholder="Senha">
+                <br><br>
+                <button id="botao">Entrar</button>
+                <h1 id="cadastrar-se">Ainda não tem uma conta? Cadastre-se</h1>
+            </div>
+        </section>
+    </body>
+    </head>
+</html>
---
+++ src/View/tela_produto.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tela Produto</title>
+    <link rel="stylesheet" type="text/css"  href="../css/main.css">
+</head>
+<body background="">
+<header class="menu-principal">
+                <main>
+                    <div class="header-1">
+                        <div class="logo_1">
+                             <img src="../img/logo_1.png">
+                        </div>
+                    </div> 
+                </main>
+                </header>
+    <section id="tela_produto"> 
+    <img src="../img/poster1.jpg" width="350">
+        
+            <article id="poster">
+                <h3>Poster Pulp Fiction</h3>
+                <h2>R$20,99</h2>
+                <button>Adicionar ao carrinho</button>
+            </article>
+    </section>
+  
+</body>
+</html>
